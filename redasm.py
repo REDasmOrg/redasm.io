97e5a48139e7c6b1bc4afc15e3823a71795c38bb
Dax <trogu.davide@gmail.com>
2019-03-03 11:18:34 +0100

    Minor fixes

--- redasm.py
+++ redasm.py
@@ -2,18 +2,23 @@ from flask import *
 from website import *
 import os
 
-app = Flask(__name__)
+application = Flask(__name__)
+
+if os.getenv("REDASM_TESTMODE"):
+    print("WARNING: Running redasm.io in TEST MODE")
+    application.jinja_env.auto_reload = True
+
 website = WebSite()
 
-@app.route('/favicon.ico')
+@application.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(application.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route("/")
+@application.route("/")
 def page_main():
     return render_template("index.html", website=website, page="/")
 
-@app.route("/<path:subpath>")
+@application.route("/<path:subpath>")
 def page_index(subpath):
     if subpath == "wiki":
         return redirect(website.wiki_url)
@@ -25,5 +30,4 @@ def page_index(subpath):
         return render_template("index.html", website=website, page=subpath)
 
 if __name__ == "__main__":
-    app.jinja_env.auto_reload = True
-    app.run()
+    application.run()
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+export REDASM_TESTMODE=1
 export FLASK_APP=redasm.py
 export FLASK_ENV=development 
 flask run
